853d8b32b9813bd7d275ce3d7655173f7b9cd5c3
Jeffrey Hurst <jhurst03@mail.bbk.ac.uk>
2015-07-23 17:22:25 +0100

    Start cachematrix.R
    Added notes, started basic code from examples to modify.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creates the inverse of a matrix if the matrix doesn't exist
+## already in the global enviroment
 
-## Write a short comment describing this function
+## creates inverse matrix and saves it to the global enviroment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  solve(x) ##expand
 }
 
-
-## Write a short comment describing this function
+## Checks if Matrix exists, is the same and retrieves if true.
+## else creates inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$matrix() ## check if matrix is there
+  if(!is.null(m)) { ##test to check if matrix exists (elaborate)
+    message("getting cached data")
+    return(m)
+  }
+  ##create inverse matrix
 }
